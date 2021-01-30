d5af5d06037cd72183b8f6667e40f4f0056c413e
insberr <jaytaygame@gmail.com>
2021-01-30 10:49:37 -0800

    removed rest api testing

--- src/rest/rest.v
+++ src/rest/rest.v
@@ -1,10 +1,11 @@
 module rest
-
+/*
 import net.http
 
 pub fn send(content string) {
 	http.post_json('https://api.hiven.io/v1/rooms/$id/messages')
 }
+*/
 pub fn test() {
 	println('rest tested (not really)')
 }
