b2218d9f0ccd5509d52485482047c52ddc651d92
insberr <jaytaygame@gmail.com>
2021-01-30 17:17:33 -0800

    started work on the rest api stuff

--- src/rest/rest.v
+++ src/rest/rest.v
@@ -1,12 +1,13 @@
 module rest
 
-/*
 import net.http
 
-pub fn send(content string) {
-	http.post_json('https://api.hiven.io/v1/rooms/$id/messages')
+pub fn send(room_id string, content string, token string) {
+	mut res := http.post_json('https://api.hiven.io/v1/rooms/$room_id/messages',
+		'{"headers": {"Authorization": "$token"},"body": {"content":"$content"}}') or { return }
+	println(res)
 }
-*/
+
 pub fn test() {
 	println('rest tested (not really)')
 }
